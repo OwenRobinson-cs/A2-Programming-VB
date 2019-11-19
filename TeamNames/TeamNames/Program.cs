7771ccf28a6a246db07b92e32863da583dea10ab
07OwenRobinson <OwenRobinson@online.sch.im>
2019-11-19 14:44:41 +0000

    fin program

---
+++ TeamNames/TeamNames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TeamNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] TeamNames = new string[16];
+            string fileName = @"TeamNames.txt";
+
+            for (int i = 0; i < TeamNames.Length; i++)
+            {
+                Console.WriteLine("Enter Team name {0}", i + 1);
+                TeamNames[i] = Console.ReadLine();
+            }
+
+            using (StreamWriter CurrentFile = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < TeamNames.Length; i++)
+                {
+                    CurrentFile.WriteLine(TeamNames[i]);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("File has been Created and Saved.");
+            Console.ReadLine();
+        }
+
+    }
+}
