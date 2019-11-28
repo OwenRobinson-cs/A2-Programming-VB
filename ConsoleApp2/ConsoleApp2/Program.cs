a765275d330adb647c27998d45d0e85bee064ec1
07OwenRobinson <OwenRobinson@online.sch.im>
2019-11-28 11:47:16 +0000

    Written Question Outpoooototototo

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -21,8 +21,14 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
+            int score;
             LoadQuestions();
+            Console.Clear();
+            Console.WriteLine("General knowladge multiple choice quiz");
+            Console.WriteLine("______________________________________");
+            Console.WriteLine("Press Any Key to begin the quiz");
             Console.ReadKey();
+            PlayQuiz(out score);
         }
 
         static void LoadQuestions()
@@ -51,5 +57,65 @@ namespace ConsoleApp2
                 }
             }
         }
+
+        static void PlayQuiz(out int score)
+        {
+            int[] NumbersUsed = new int[10];
+            int CurrentNumber = 0;
+            score = 0;
+            Random RNG = new Random(30);
+            CurrentNumber = RNG.Next(30);
+            char CorrectLetter;
+            //Ask Question
+            for (int i = 0; i < 10; i++)
+            {
+
+                while (CurrentNumber == NumbersUsed[0] || CurrentNumber == NumbersUsed[1] || CurrentNumber == NumbersUsed[2] || CurrentNumber == NumbersUsed[3] || CurrentNumber == NumbersUsed[4] || CurrentNumber == NumbersUsed[5] || CurrentNumber == NumbersUsed[6] || CurrentNumber == NumbersUsed[7] || CurrentNumber == NumbersUsed[8]  || CurrentNumber == NumbersUsed[9] || CurrentNumber == 0)
+                {
+                    CurrentNumber = RNG.Next(30);
+                }
+
+                Console.WriteLine(Questions[CurrentNumber].Question);
+                NumbersUsed[i] = CurrentNumber;
+                //output all possible answers Randomly
+                Random RNG1 = new Random(4);
+                int AnswerOrder = RNG1.Next(4);
+                if (AnswerOrder == 1)
+                {
+                    Console.WriteLine(Questions[CurrentNumber].Answer1);
+                    Console.WriteLine(Questions[CurrentNumber].Answer2);
+                    Console.WriteLine(Questions[CurrentNumber].Answer3);
+                    Console.WriteLine(Questions[CurrentNumber].CorrectAnswer);
+                    CorrectLetter = 'D';
+
+                }
+                else if (AnswerOrder == 2)
+                {
+                    Console.WriteLine(Questions[CurrentNumber].Answer3);
+                    Console.WriteLine(Questions[CurrentNumber].CorrectAnswer);
+                    Console.WriteLine(Questions[CurrentNumber].Answer2);
+                    Console.WriteLine(Questions[CurrentNumber].Answer1);
+                    CorrectLetter = 'B';
+                }
+                else if (AnswerOrder == 4)
+                {
+
+                    Console.WriteLine(Questions[CurrentNumber].Answer2);
+                    Console.WriteLine(Questions[CurrentNumber].Answer1);
+                    Console.WriteLine(Questions[CurrentNumber].CorrectAnswer);
+                    Console.WriteLine(Questions[CurrentNumber].Answer3);
+                    CorrectLetter = 'C';
+                }
+                else
+                {
+                    Console.WriteLine(Questions[CurrentNumber].CorrectAnswer);
+                    Console.WriteLine(Questions[CurrentNumber].Answer3);
+                    Console.WriteLine(Questions[CurrentNumber].Answer1);
+                    Console.WriteLine(Questions[CurrentNumber].Answer2);
+                    CorrectLetter = 'A';
+                }
+            }
+        }
+
     }
 }
