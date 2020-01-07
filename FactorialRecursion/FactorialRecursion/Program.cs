7cdc6e897c0a4b753852793e89d60158bb0a1294
07OwenRobinson <OwenRobinson@online.sch.im>
2020-01-07 15:10:55 +0000

    Commenter Pro Sim 2020

--- FactorialRecursion/FactorialRecursion/Program.cs
+++ FactorialRecursion/FactorialRecursion/Program.cs
@@ -16,22 +16,23 @@ namespace FactorialRecursion
             Console.WriteLine("Enter Number: ");
             int.TryParse(Console.ReadLine(), out num);
 
-            answer = FactorialN(num);
+            answer = FactorialN(num); //Calls Function
 
-            Console.WriteLine(answer);
+            Console.WriteLine(answer); //Outputs answer
             Console.ReadKey();
         }
 
         static int FactorialN(int num)
         {
             int answer;
-            if (num == 1)
+            if (num != 1)   //Checks to make sure input wasn't 1
             {
-                answer = 1;
+                answer = num * FactorialN(num - 1); //Calls itself while removing 1 from num
             }
+
             else
             {
-                answer = num * FactorialN(num - 1);
+                answer = 1; 
             }
 
             return answer;
