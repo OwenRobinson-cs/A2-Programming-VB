ad28b4b6b6bffa1f9c9e70c9a329ca83e9dadb7d
07owenrobinson <07owenrobinson@RGS632605>
2020-01-28 15:29:02 +0000

    Completed program

---
+++ LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+
+           public static void Main(string[] args)
+            {
+                int[] NumberArray = { 77, 5, 3, 78, 9, 90, 34, 36, 500, 79, 1, 7, 66, 21, 92, 455, 46 };
+                int searchNo;
+
+                Console.Write("What number do you want to find: ");
+                int.TryParse(Console.ReadLine(), out searchNo);
+
+                Console.WriteLine(LinearSearch(NumberArray, searchNo));
+                Console.ReadKey();
+            }
+
+            public static int LinearSearch(int[] inputArray, int key)
+            {
+                int location = 0;
+                bool found = false;
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                        if (key == inputArray[i])
+                        {
+                               Console.WriteLine("Number Found");
+                               location = i;
+                               found = true;
+                               break;
+
+                        }
+                }
+
+                        if (found != true)
+                        {
+                       Console.WriteLine("Number not found.");
+                        }
+
+                     return location;
+            }
+
+       
+    }
+}
---
+++ MergeSortLetter/MergeSortLetter/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortLetter
+{
+    class Program
+    {
+        public const int AmountofData = 1000000;
+        static void Main(string[] args)
+        {
+            char[] UnsortedArray = new char[AmountofData];
+            UnsortedArray = FillArray();
+
+            Console.WriteLine("Merge Sort \n \n");
+
+            //Console.WriteLine("Unsorted Letters: ");
+
+            //for (int i = 0; i < AmountofData; i++)
+            //{
+            //    Console.Write("{0}, ", UnsortedArray[i]);
+            //}
+            //Console.WriteLine("\n \n");
+
+            DateTime StartTime = DateTime.Now;
+            MergeSort(UnsortedArray, 0, UnsortedArray.Length - 1);
+            DateTime EndTime = DateTime.Now;
+            TimeSpan TimeTaken = EndTime - StartTime;
+
+            Console.WriteLine("Sorted Letters: ");
+
+            for (int i = 0; i < AmountofData; i++)
+            {
+                Console.Write("{0}, ", UnsortedArray[i]);
+            }
+            Console.WriteLine("\n \n");
+
+            Console.WriteLine("The Program took {0}ms to execute", TimeTaken.TotalMilliseconds);
+            Console.ReadKey();
+        }
+
+        static char[] FillArray()
+        {
+            char[] TempArray = new char[AmountofData];
+            Random RNG = new Random();
+            for (int i = 0; i < AmountofData; i++)
+            {
+                TempArray[i] = (Char)('a' + RNG.Next(0, 26));
+            }
+
+            return TempArray;
+        }
+
+
+        static public void MergeSort(char[] letters, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort(letters, left, mid); //recursive call 1
+                MergeSort(letters, (mid + 1), right); //recursive call 2
+
+                DoMerge(letters, left, (mid + 1), right);
+            }
+        }
+
+        static public void DoMerge(char[] letters, int left, int mid, int right)
+        {
+            char[] temp = new char[letters.Length];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (letters[left] <= letters[mid])
+                    temp[tmp_pos++] = letters[left++];
+                else
+                    temp[tmp_pos++] = letters[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = letters[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = letters[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                letters[right] = temp[right];
+                //Console.Write(letters[right].ToString().PadLeft(3)); //delete me
+                right--;
+            }
+            //  Console.WriteLine();
+        }
+
+    }
+
+}
+
