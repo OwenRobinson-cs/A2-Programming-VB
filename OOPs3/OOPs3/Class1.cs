65b24643e2aaeb534f67aca9bdeb4400ba2dd0f1
owen robinson <owenrobinson@manx.net>
2020-03-23 18:56:34 +0000

    Completed all tasks (1 2 and 3)

---
+++ OOPs3/OOPs3/Class1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs3
+{
+    class Animal
+    {
+        private string name;
+        private string gender;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+
+        }
+
+        public Animal()
+        {
+            SetGender();
+            NameAnimal(name);
+        }
+        public Animal(string _name)
+        {
+            SetGender();
+            name = _name;
+        }
+
+        virtual public void NameAnimal(string _name)
+        {
+            name = Console.ReadLine();
+        }
+
+        virtual public string MakeNoise(int happynessDefiner)
+        {
+            if (happynessDefiner < 5) { return "Unhappy Gurgle"; }
+            else { return "Gurgle"; }
+        }
+
+        private void SetGender()
+        {
+            Random rnd = new Random(216);
+            if (rnd.Next(0, 500) % 2 == 1)
+            {
+                this.gender = "male";
+            }
+            else
+            {
+                this.gender = "female";
+            }
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat()
+            : base()
+        {
+
+        }
+        public Cat(string name)
+            : base(name)
+        {
+
+        }
+
+        public override void NameAnimal(string _name)
+        {
+
+            Console.WriteLine("What is the name of your cat?");
+
+            base.NameAnimal(_name);
+        }
+        new public string MakeNoise(int happynessDefiner)
+        {
+            if (happynessDefiner < 5) { return "Hiss"; }
+            else { return "Meow"; }
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog()
+            : base()
+        {
+
+        }
+        public Dog(string name)
+            : base(name)
+        {
+
+        }
+
+        public override void NameAnimal(string _name)
+        {
+
+            Console.WriteLine("What is the name of your dog?");
+
+            base.NameAnimal(_name);
+        }
+        new public string MakeNoise(int happynessDefiner)
+        {
+            if (happynessDefiner < 5) { return "Growl";  }
+            else { return "Bark";  }
+        }
+    }
+
+}
---
+++ OOPs3/OOPs3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal pet1 = new Animal("Sam");
+            Cat pet2 = new Cat("Bob");
+            Dog pet3 = new Dog();
+            int happynessDefiner;
+
+            Console.WriteLine("{0} is {1}", pet1.Name, pet1.Gender);
+            Console.WriteLine("Enter a number relating to the happyness of {0}", pet1.Name);
+            int.TryParse(Console.ReadLine(), out happynessDefiner);
+            Console.WriteLine(pet1.MakeNoise(happynessDefiner));
+
+            Console.WriteLine("{0} is {1}", pet2.Name, pet2.Gender);
+            Console.WriteLine("Enter a number relating to the happyness of {0}", pet2.Name);
+            int.TryParse(Console.ReadLine(), out happynessDefiner);
+            Console.WriteLine(pet2.MakeNoise(happynessDefiner));
+
+            Console.WriteLine("{0} is {1}", pet3.Name, pet3.Gender);
+            Console.WriteLine("Enter a number relating to the happyness of {0}", pet3.Name);
+            int.TryParse(Console.ReadLine(), out happynessDefiner);
+            Console.WriteLine(pet3.MakeNoise(happynessDefiner));
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
